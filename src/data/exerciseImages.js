2b3a61ef5061ab3ac5d353966fa306ba833654c9
Sofiia Shabashova <s0596669@htw-berlin.de>
2026-07-01 23:52:29 +0200

    exercises

---
+++ src/data/exerciseImages.js
@@ -0,0 +1,21 @@
+export const exerciseImages = {
+  "neck-mobility": require("../assets/images/exercises/neck-mobility.png"),
+  "chin-tucks": require("../assets/images/exercises/chin-tucks.png"),
+  "neck-stretch": require("../assets/images/exercises/neck-stretch.png"),
+  "shoulder-rolls": require("../assets/images/exercises/shoulder-rolls.png"),
+  "wall-angels": require("../assets/images/exercises/wall-angels.png"),
+  "chest-stretch": require("../assets/images/exercises/chest-stretch.png"),
+  "arm-circles": require("../assets/images/exercises/arm-circles.png"),
+  "seated-row-motion": require("../assets/images/exercises/seated-row-motion.png"),
+  "seated-thoracic-extension": require("../assets/images/exercises/seated-thoracic-extension.png"),
+  "standing-pelvic-tilts": require("../assets/images/exercises/standing-pelvic-tilts.png"),
+  "seated-knee-lift": require("../assets/images/exercises/seated-knee-lift.png"),
+  "standing-forward-reach": require("../assets/images/exercises/standing-forward-reach.png"),
+  "hip-circles": require("../assets/images/exercises/hip-circles.png"),
+  "standing-leg-abduction": require("../assets/images/exercises/standing-leg-abduction.png"),
+  "standing-hip-flexor-stretch": require("../assets/images/exercises/standing-hip-flexor-stretch.png"),
+  "march-in-place": require("../assets/images/exercises/march-in-place.png"),
+  "squats": require("../assets/images/exercises/squats.png"),
+  "standing-hamstring-stretch": require("../assets/images/exercises/standing-hamstring-stretch.png")
+
+};
